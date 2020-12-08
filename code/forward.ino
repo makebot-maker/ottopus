c13555b6b1a649cc3c2c3942182cd33c18789388
WONDAEHAN <62107740+makebot-maker@users.noreply.github.com>
2020-12-09 08:38:01 +0900

    Update forward.ino

--- code/forward.ino
+++ code/forward.ino
@@ -1,3 +1,9 @@
+/*makebot ottopus code
+ * makebot homepage(English) : http://makebot.mystrikingly.com/
+ * makebot homepage(Korean) : https://makebot.modoo.at
+ * files : https://github.com/makebot-lab/ottopus
+ * made by WONDAEHAN.
+ */
 #include <Ottopus.h>
 
 Ottopus Ottopus;
